be188198eff56a168c1283339c3ae45e96624e0a
Borna Miljković <borna.milja@gmail.com>
2025-09-22 14:23:02 +0200

    api ,axios library,https://loading.io/css/ za loading spinners optimizirane i error handling

--- src/components/Admin/Sellers.jsx
+++ src/components/Admin/Sellers.jsx
@@ -1,17 +1,33 @@
 import { useEffect, useState } from "react";
-
+import axios from "axios";
+import Loader from "../Common/Loader";
 const Sellers = () => {
   const [name, setName] = useState("");
+  const [sellers, setSellers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
   useEffect(() => {
-    console.log("Sellers mount");
-    return () => {
-      console.log("Sellers Unmount");
-    };
-  }, [name]);
+    setIsLoading(true);
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setSellers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
+      });
+  }, []);
   return (
     <>
       <h3>Admin Sellers Page</h3>
       <input type="text" onChange={(e) => setName(e.target.value)} />
+      {isLoading && <Loader />}
+      {error && <em>{error}</em>}
+      {sellers.map((seller) => (
+        <p key={seller.id}>{seller.name}</p>
+      ))}
     </>
   );
 };
---
+++ src/components/Common/Loader.jsx
@@ -0,0 +1,13 @@
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div>
+      <div className="lds-circle">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
